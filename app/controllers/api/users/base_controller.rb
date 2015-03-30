3c8d90cba2372905761473465e65cbcfd79f9ea1
Antonio Chavez <cavjzz@gmail.com>
2015-03-29 23:17:27 -0600

    Feature/sessions
    * Add required fields for user registration
    * Add factory girl
    * Add step defintion for feature
    * Add create sessions endpoint and model methods
    * Refactor code
    * Add destroy session endpoint
    * Skip unecessary controllers

--- app/controllers/api/users/base_controller.rb
+++ app/controllers/api/users/base_controller.rb
@@ -1,6 +1,15 @@
 module Api
   module Users
     class BaseController < ::Api::BaseController
+      before_filter :find_and_authenticate_user!
+
+      private
+
+      def find_and_authenticate_user!
+        user = User.find_by!(authentication_token: params[:authentication_token])
+        sign_in :user, user, store: false
+        authenticate_user!
+      end
     end
   end
 end
--- app/controllers/api/users/registrations_controller.rb
+++ app/controllers/api/users/registrations_controller.rb
@@ -1,26 +1,28 @@
 module Api
   module Users
     class RegistrationsController < ::Api::Users::BaseController
+      skip_before_filter :find_and_authenticate_user!, only: :create
+
       rescue_from ActiveRecord::RecordInvalid do |e|
         render json: { user: { errors: e.message } },
           status: :unprocessable_entity
       end
 
       def create
-        @user = User.register(user_params)
+        @user = User.register!(user_params)
         render :create, status: :created
       end
 
       private
 
       def user_params
-        params.require(:user).permit(:name,
-                                     :email,
-                                     :username,
-                                     :phone_number,
-                                     :profile_picture,
-                                     :password,
-                                     :password_confirmation)
+        params.fetch(:user, {}).permit(:name,
+                                       :email,
+                                       :username,
+                                       :phone_number,
+                                       :profile_picture,
+                                       :password,
+                                       :password_confirmation)
       end
     end
   end
