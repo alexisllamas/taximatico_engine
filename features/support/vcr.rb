dfc19d14648d5405e3e075150df02c87b308ab6f
Antonio Chavez <cavjzz@gmail.com>
2015-05-04 13:22:53 -0500

    Ignore codeclimate vcr

--- features/support/vcr.rb
+++ features/support/vcr.rb
@@ -6,4 +6,5 @@ VCR.configure do |c|
   c.hook_into :webmock
   c.cassette_library_dir = 'features/cassettes'
   c.ignore_localhost = true
+  c.ignore_hosts 'codeclimate.com'
 end
--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,5 +1,6 @@
 VCR.configure do |config|
   config.cassette_library_dir = "spec/support/fixtures/vcr_cassettes"
+  config.ignore_hosts 'codeclimate.com'
   config.hook_into :webmock
   config.configure_rspec_metadata!
 end
